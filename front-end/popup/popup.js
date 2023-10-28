85173b70b0b7c06647e72ccb8c38262be8ed7fb7
Jonathan Nguyen <jonathanguven@gmail.com>
2023-10-27 21:18:02 -0700

    epic restructure again

--- front-end/popup/popup.js
+++ front-end/popup/popup.js
@@ -1,1 +1,9 @@
-console.log('sup');
+const countP = () => {
+    var pTags = document.querySelectorAll('p');
+    return pTags.length;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var count = counP();
+    document.querySelector('#count').innerHTML = count;
+})
